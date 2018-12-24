2eb6b23e525cba9c784d3c9d683eff1ac3530119
Akarin9 <9tangyue9@gmail.com>
2018-12-24 20:12:23 +0800

    update

---
+++ src/main/java/com/soft1841/cn/dao/impl/TypeDAOImpl.java
@@ -0,0 +1,36 @@
+package com.soft1841.cn.dao.impl;
+
+import cn.hutool.db.Db;
+import com.soft1841.cn.dao.TypeDAO;
+import com.soft1841.cn.entity.Type;
+import cn.hutool.db.Entity;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class TypeDAOImpl implements TypeDAO {
+    @Override
+    public Long insertType(Type type) throws SQLException {
+        return Db.use().insertForGeneratedKey(
+                cn.hutool.db.Entity.create("t_type")
+                        .set("type_name", type.getTypeName())
+        );
+    }
+
+    @Override
+    public int deleteTypeById(long id) throws SQLException {
+        return Db.use().del(
+                cn.hutool.db.Entity.create("t_type").set("id", id)
+        );
+    }
+
+    @Override
+    public List<Entity> selectAllTypes() throws SQLException {
+        return Db.use().query("SELECT * FROM t_type ");
+    }
+
+    @Override
+    public Entity getTypeById(int id) throws SQLException {
+        return Db.use().queryOne("SELECT * FROM t_type WHERE id = ? ", id);
+    }
+}
--- src/main/java/com/soft1841/cn/utils/DAOFactory.java
+++ src/main/java/com/soft1841/cn/utils/DAOFactory.java
@@ -1,7 +1,9 @@
 package com.soft1841.cn.utils;
 
 import com.soft1841.cn.dao.SellerDAO;
+import com.soft1841.cn.dao.TypeDAO;
 import com.soft1841.cn.dao.impl.SellerDAOImpl;
+import com.soft1841.cn.dao.impl.TypeDAOImpl;
 
 /**
  * DAO工厂类
@@ -10,4 +12,9 @@ public class DAOFactory {
     public static SellerDAO getSellerDAOInstance() {
         return new SellerDAOImpl();
     }
+
+    public static TypeDAO getTypeDAOInstance() {
+        return new TypeDAOImpl();
+    }
+
 }
