27af060279adad91480668f0fd19a273c72c0266
Akarin9 <9tangyue9@gmail.com>
2018-12-25 13:54:30 +0800

    update goods

--- src/main/java/com/soft1841/cn/utils/DAOFactory.java
+++ src/main/java/com/soft1841/cn/utils/DAOFactory.java
@@ -17,8 +17,10 @@ public class DAOFactory {
 
     public static AdminDAO getAdminDAOInstance() { return new AdminDAOImpl(); }
 
-    public static MemberDAO getMamberDAOInstance() { return new MemberDAOImpl(); }
+    public static MemberDAO getMemberDAOInstance() { return new MemberDAOImpl(); }
 
     public static DetailDAO getDetailDAOInstance(){return new DetailDAOImpl();}
+
+    public static GoodsDAO getGoodsDAOInstance(){return new GoodsDAOImpl();}
 }
 
